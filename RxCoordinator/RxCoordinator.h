f0f3332ffedf7e648e7fc495db28966c04002033
Andrew <andruvs@gmail.com>
2020-12-21 17:11:20 +0300

    Initial project setup

---
+++ RxCoordinator/RxCoordinator.h
@@ -0,0 +1,18 @@
+//
+//  RxCoordinator.h
+//  RxCoordinator
+//
+//  Created by Andrey Golovchak on 21.12.2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RxCoordinator.
+FOUNDATION_EXPORT double RxCoordinatorVersionNumber;
+
+//! Project version string for RxCoordinator.
+FOUNDATION_EXPORT const unsigned char RxCoordinatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxCoordinator/PublicHeader.h>
+
+
